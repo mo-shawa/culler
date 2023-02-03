2a6d666c1db45e931c611e1d2cb24c317f25b8c6
Mahmoud El-Shawa <moshawa0@gmail.com>
2023-02-03 11:47:52 -0500

    working numGen

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports.genRGBA = function () {
+	const [r, g, b, a] = [
+		genNumBetween(0, 255),
+		genNumBetween(0, 255),
+		genNumBetween(0, 255),
+		genNumBetween(0, 1, true),
+	]
+
+	return `rgba(${r}, ${g}, ${b}, ${a})`
+}
+
+function genNumBetween(min, max, isFloat = false) {
+	const num = Math.random() * max - min
+	return isFloat ? num : Math.floor(num)
+}
+
+console.log(genNumBetween(0, 1, true))
