3483a64ac01b3230258eb3bebca6a64d87e41626
Mahmoud El-Shawa <moshawa0@gmail.com>
2023-02-03 12:25:38 -0500

    convert to ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-module.exports.genRGBA = function () {
+export function genRGBA() {
 	const [r, g, b, a] = [
 		genNumBetween(0, 255),
 		genNumBetween(0, 255),
@@ -9,9 +9,9 @@ module.exports.genRGBA = function () {
 	return `rgba(${r}, ${g}, ${b}, ${a})`
 }
 
-function genNumBetween(min, max, isFloat = false) {
+function genNumBetween(min: number, max: number, isFloat = false): number {
 	const num = Math.random() * max - min
-	return isFloat ? num : Math.floor(num)
+	return isFloat ? num : Math.floor(num) // might wanna reduce to 2 decimal places
 }
 
 console.log(genNumBetween(0, 1, true))
